7bab635f47ba1811da951b3564477b05e78a1419
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-13 12:30:28 -0700

    parse scale

--- js/aside.js
+++ js/aside.js
@@ -5,12 +5,22 @@ const $ = document;
 export function renderAside() {
 	const asideContainer = $.querySelector('aside .container');
 
+	const _renderScale = (arr) => {
+		return arr
+			.map((item) => `<span>${item}</span>`)
+			.join(',')
+			.replace(',', '');
+	};
+
 	qualifications.forEach((qualification, index) => {
 		const { title, scale } = qualification;
 		//const qualificationEl = $.createElement('section');
 		const qualificationEl = `
 			<section class="qualification qualification-${title.toLowerCase()}">
-				<h4>${title}</h4>
+				<div class="qualification__title">
+					<h4>${title}</h4>
+					${scale ? `<p>${_renderScale(scale)}</p>` : ''}
+				</div>
 			</section>
 		`;
 
