0a002f1646de3aa097426ddc3853143a76b38279
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-13 00:15:41 -0700

    Animate scroll track

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-import { renderTimeline } from './timeline.js';
+import { renderTimeline, renderTrack } from './timeline.js';
 
 window.onload = function () {
 	console.log('--Javascript Loaded');
 	renderTimeline();
+	renderTrack();
 };
--- js/timeline.js
+++ js/timeline.js
@@ -1,8 +1,11 @@
 import history from './history.js';
 
+const $ = document;
+const historySelector = '.timeline .container .history';
+
 export function renderTimeline() {
 	console.log('--Render Timeline History');
-	const containerEl = document.querySelector('.timeline .container .history');
+	const containerEl = $.querySelector(historySelector);
 
 	history.forEach((item, index) => {
 		const { name, description, from, to } = item;
@@ -15,5 +18,39 @@ export function renderTimeline() {
 		`;
 
 		containerEl.insertAdjacentHTML('beforeend', markup);
+	});
+}
+
+export function renderTrack() {
+	const containerEl = $.querySelector(historySelector);
+	const footerEl = $.querySelector('footer');
+
+	const trackContainer = $.createElement('div');
+	trackContainer.classList.add('track');
+
+	const trackSlide = $.createElement('div');
+	trackSlide.classList.add('track__slide');
+
+	trackContainer.append(trackSlide);
+
+	containerEl.append(trackContainer);
+
+	const buffer = 200;
+	window.addEventListener('scroll', (e) => {
+		const trackContainerBounds = trackContainer.getBoundingClientRect();
+		const footerBounds = trackContainer.getBoundingClientRect();
+
+		if (trackContainerBounds.y <= buffer) {
+			trackSlide.style.height =
+				((window.scrollY - trackContainerBounds.y) / trackContainerBounds.height) * 100 + '%';
+		}
+
+		if (trackContainerBounds.y <= buffer && footerBounds.bottom < window.innerHeight) {
+			trackSlide.style.height = '100%';
+		}
+
+		if (trackContainerBounds.y > 200) {
+			trackSlide.style.height = '0%';
+		}
 	});
 }
