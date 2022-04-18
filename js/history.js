14ecc182f01d4492c4482f60421cd92d71b89298
Nicholas Eli <nicholaseli@Nicholass-MacBook-Air.local>
2022-04-18 09:48:22 -0700

    Update history

--- js/history.js
+++ js/history.js
@@ -2,35 +2,35 @@ export default [
 	{
 		name: 'Rock Paper Coin',
 		description:
-			'Develop systems by studying operations; designing, developing and installing software solutions; support and develop software team. Lead a team of developers responsible for building new features and support existing platform using Node JS, React/React Native and Amazon Web Services.',
+			'Develop systems by studying operations; designing, developing and installing software solutions; support and develop software team.',
 		from: 2020,
 		to: 2022,
 	},
 	{
 		name: 'Single Mind Consulting',
 		description:
-			'Build web and mobile application environments using React, React Native that communicate with serverless technologies such as AWS, Azure or Firebase. Serverless usage ranges from machine learning, data and file storage, geolocation and authentication services. Using Node as in intermediary environment to relay data to client side web or mobile apps modeled in a consumable format or verify token authentication before service transactions are made.',
+			'Build standalone web and mobile front ends  that communicate with a serverless API layer.  SingleMind is a full service shop which requires the ability to manage local environments to any dev ops or deployment process.',
 		from: 2017,
 		to: 2019,
 	},
 	{
 		name: 'Platt Electric',
 		description:
-			'Build internal and external web applications in pure JavaScript or Vue JS inside the .NET framework.  Projects were completed in two week sprint cycles.  Each project ranged in category from the business facing website, mobile site and app, customer tools and credit, product inventory management and sales, employee education, data collection, research and development of existing and future technology.',
+			'Build internal and external tools and consumer based products with web or mobile front end that uses .NET as an API layer.  Additionally discover how to use existing technology through research and development for customer facing applications.',
 		from: 2017,
 		to: 2017,
 	},
 	{
 		name: 'Pairodime',
 		description:
-			'Create Drupal themes for some of the top brands in the snowboard industry and other counter culture style brands. The projects are generally large scale e-commerce sites with brand heavy design, animation and layout personality for end users.  For the admin there is a vast amount of functionality to manage emails, warranty requests, sales reports, inventory, shipping and other customer interaction.<br/><br/>The sites refreshed every season only changing visually with new features and maintaining legacy database. This creates the exceptional challenge of making sure displayed data is as accurate as previous season.',
+			'Build and maintain a catalog of various ecommerce sites that are a part of a large parent company. Each season the task was to take the newly designed catalog and convert it to the Drupal platform with new and past line items.',
 		from: 2015,
 		to: 2017,
 	},
 	{
 		name: 'Formations Design Group',
 		description:
-			'Develop and extend Joomla! themes for various clients from small business to large corporate clients and local government. Theme development generally consisted of modifying premium templates that came with their own CSS framework and existing JavaScript functionality.<br/><br/>Because this approach caused various road blocks with the development process, I created our own in house template system that had multiple backend features for managing the theme display.',
+			'Develop and extend Joomla! themes for various clients from small business, corporate  and local government. ',
 		from: 2013,
 		to: 2014,
 	},
