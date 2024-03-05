21f2937c1175082c8ed6a7530a9de22c6a93f911
NicholasEli <nicholasbydesign@gmail.com>
2024-03-05 12:01:00 -0800

    clean slate

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
-import {
-	renderTimeline,
-	renderTrack,
-	renderPoints,
-	renderFlags,
-	trackScrollEvents,
-} from './timeline.js';
-
-import { renderAside, asideScrollEvents } from './aside.js';
-
 window.onload = function () {
 	console.log('--Javascript Loading');
 	// Timeline / Main
-	renderTimeline();
-	renderTrack();
-	renderPoints();
-	renderFlags();
-	trackScrollEvents();
 
-	// Aside
-	renderAside();
-	asideScrollEvents();
 	console.log('--Javascript Loaded');
 };
